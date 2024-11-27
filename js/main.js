42529f1c1c6c00e586499fca555d658a3aae0bb4
Dalia-Fathy <daliafathy2004@gmail.com>
2024-11-27 20:13:03 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,34 +1,105 @@
-var quotes = [
-  {
-    author: "-- Oscar Wilde",
-    quotes: "“Be yourself; everyone else is already taken.”"
-  },
+var BookmarkName = document.getElementById("bookmarkName")
+var WebsiteURL = document.getElementById("bookmarkURL")
+var submitInbut = document.getElementById("submitBtn")
+var mainIndex = 0;
+var searchInput = document.getElementById("searchInput")
 
-  {
-    author: "-- Frank Zappa",
-    quotes: "“So many books, so little time.”"
-  },
+var bookmark = [];
+if (localStorage.getItem("Data") != null) {
+    bookmark = JSON.parse(localStorage.getItem("Data"))
+    display()
+}
 
-  {
-    author: "-- Marcus Tullius Cicero",
-    quotes: "“A room without books is like a body without a soul.”"
-  },
-
-  {
-    author: "-- Albert Einstein",
-    quotes: "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”"
-  },
-  {
-    author:"-- Friedrich Nietzsche,",
-    quotes:"“Without music, life would be a mistake.”"
-  },
-  {
-    author:"-- Allen Saunders",
-    quotes:"“Life is what happens to us while we are making other plans.”"
-  }
-]
-function newQuote(){
-  var randomNumber = Math.floor(Math.random() * quotes.length);
-  document.getElementById("outputquotes").innerHTML=quotes[randomNumber].quotes;
- document.getElementById("outputauthor").innerHTML=quotes[randomNumber].author;
+function submit() {
+    var site = {
+        name: BookmarkName.value,
+        url: WebsiteURL.value
     }
+    if (submitInbut.innerHTML == "Submit") {
+        bookmark.push(site);
+    }
+    else {
+        bookmark.splice(mainIndex, 1, site)
+        submitInbut.innerHTML = "Submit"
+    }
+    localStorage.setItem("Data", JSON.stringify(bookmark))
+    display()
+}
+
+
+function display() {
+    var temp = ""
+    for (var i = 1; i <= bookmark.length; i++) {
+        temp += `  <tr>
+                    <td>`+ i + `</td>
+                    <td>`+ bookmark[i - 1].name + `</td>
+                    <td>
+                     <a href="`+ bookmark[i - 1].url + `" target="_blank">
+                         <button class="btn btn-visit pe-2" ">
+                            <i class="fa-solid fa-eye"></i>
+                            Visit
+                        </button>
+                     </a>
+                    
+                    </td>
+                    <td>
+                        <button class="btn btn-delete pe-2" onclick="deleteProduct(`+ [i - 1] + `)" >
+                            <i class="fa-solid fa-trash-can"></i>
+                            Delete
+                        </button>
+                    </td>
+                     <td>
+                            <button class=" btn btn-warning" onclick="updateProduct(`+ [i - 1] + `)">update</button>
+                        </td>
+                </tr>`
+
+    }
+    document.getElementById("tableBody").innerHTML = temp
+    clear()
+}
+
+function search() {
+    var temp = ""
+    var searchData = searchInput.value.toLowerCase()
+    for (var i = 0; i < bookmark.length; i++) {
+        if (bookmark[i].name.toLowerCase().includes(searchData) == true) {
+            temp += ` <tr>
+                    <td>`+ i + `</td>
+                    <td>`+ bookmark[i - 1].name + `</td>
+                    <td>
+                        <button class="btn btn-visit pe-2" onclick="visit(`+ [i - 1] + `)">
+                            <i class="fa-solid fa-eye"></i>
+                            Visit
+                        </button>
+                    </td>
+                    <td>
+                        <button class="btn btn-delete pe-2" onclick="deleteProduct(`+ [i - 1] + `)" >
+                            <i class="fa-solid fa-trash-can"></i>
+                            Delete
+                        </button>
+                    </td>
+                </tr>`
+        }
+    }
+    document.getElementById("tableBody").innerHTML = temp
+}
+
+function deleteProduct(productIndex) {
+    bookmark.splice(productIndex, 1)
+    localStorage.setItem("Data", JSON.stringify(bookmark))
+    display()
+}
+
+function updateProduct(productIndex) {
+    mainIndex = productIndex;
+    var site = bookmark[productIndex];
+    BookmarkName.value = site.name
+    WebsiteURL.value = site.url
+
+    submitInbut.innerHTML = "update product"
+}
+
+function clear(){
+    BookmarkName.value = "",
+    WebsiteURL.value = ""
+}
